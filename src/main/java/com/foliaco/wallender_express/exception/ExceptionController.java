248cf968ffbd4319e907e97b75173c897b52e063
stevengodev <stevengodev@gmail.com>
2025-01-10 00:10:10 -0500

    Actualiza el controlador de excepciones

--- src/main/java/com/foliaco/wallender_express/exception/ExceptionController.java
+++ src/main/java/com/foliaco/wallender_express/exception/ExceptionController.java
@@ -1,18 +1,18 @@
 package com.foliaco.wallender_express.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler( value = { NotFoundException.class } )
-    public ResponseEntity<ErrorMessage> handleNotFoundException(RuntimeException e,
-                                                 WebRequest request) {
+    public ResponseEntity<ErrorMessage> handleNotFoundException(RuntimeException e) {
 
         ErrorMessage errorMessage = ErrorMessage.builder()
                 .message(e.getMessage())
@@ -22,6 +22,28 @@ public class ExceptionController {
 
         return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
 
+    }
+
+    @ExceptionHandler(value = { FileOperationException.class } )
+    public ResponseEntity<ErrorMessage> handleNoFileSelectedException(RuntimeException e){
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .message(e.getMessage())
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .dateTime(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(JsonProcessingException.class)
+    public ResponseEntity<ErrorMessage> handleJsonProcessingException(JsonProcessingException ex) {
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .message("An error occurred while processing the JSON data")
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .dateTime(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
---
+++ src/main/java/com/foliaco/wallender_express/exception/FileOperationException.java
@@ -0,0 +1,9 @@
+package com.foliaco.wallender_express.exception;
+
+public class FileOperationException extends RuntimeException{
+
+    public FileOperationException(String message) {
+        super(message);
+    }
+
+}
